f7a52560514af16bfb1f17e645962959bf9d0e50
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2019-11-08 10:20:57 +0900

    [ADD] summoner Page, index Page
    summoner Page와 main Page ADD

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainContainer from '../container/mainContainer';
+
+const index = () => {
+  return (
+    <MainContainer/>
+  );
+};
+
+export default index;
---
+++ pages/summoner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SummonerContainer from '../container/SummonerContainer';
+
+const summoner = () => {
+  return (
+    <div>
+      <SummonerContainer/>
+    </div>
+  );
+};
+
+export default summoner;
