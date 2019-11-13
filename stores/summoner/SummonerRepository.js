f378010bfc44e28cfb6cb8b226c91ef4bc60555b
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2019-11-13 20:08:14 +0900

    [ADD] SummonerStore, Repository
    Repository: getSummonerLank, getSummonerMatches,
    getSummonerMatchesStat 추가
    Store: 위의 함수들, getWinRatio, getMiniute 추가

--- stores/summoner/SummonerRepository.js
+++ stores/summoner/SummonerRepository.js
@@ -4,7 +4,34 @@ import axios from 'axios';
 class SummonerRepository {
   async getSummonerByName(summonerName) {
     try {
-      let summonerUrl = `${config.RIOT_URL}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${config.RIOT_API_KEY}`
+      let summonerUrl = `${config.RIOT_URL}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${config.RIOT_API_KEY}`;
+      return await axios.get(summonerUrl);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async getSummonerLank(summonerId) {
+    try {
+      let summonerUrl = `${config.RIOT_URL}/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${config.RIOT_API_KEY}`;
+      return await axios.get(summonerUrl);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async getSummonerMatches(summonerAccountId) {
+    try {
+      let summonerUrl = `${config.RIOT_URL}/lol/match/v4/matchlists/by-account/${summonerAccountId}?api_key=${config.RIOT_API_KEY}`;
+      return await axios.get(summonerUrl);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async getSummonerMatchesStat(gameId){
+    try {
+      let summonerUrl = `${config.RIOT_URL}/lol/match/v4/matches/${gameId}?api_key=${config.RIOT_API_KEY}`;
       return await axios.get(summonerUrl);
     } catch (err) {
       console.log(err);
--- stores/summoner/SummonerStore.js
+++ stores/summoner/SummonerStore.js
@@ -1,17 +1,94 @@
-import { observable, action} from 'mobx';
+import { observable, action } from 'mobx';
 import SummonerRepository from './SummonerRepository';
 
+//matchStore 만드는게 낫지 않을까.
 class SummonerStore {
-  @observable summoner = [];
+  @observable.ref summoner = [];
+  @observable.ref summonerLank = [];
+  @observable.ref matches = [];
+  // @observable.ref matchesStat = [];
+  @observable loading = false;
 
   @action
   async getSummonerByName(summonerName) {
-    try{
-      const data = await SummonerRepository.getSummonerByName(summonerName)
+    try {
+      const data = await SummonerRepository.getSummonerByName(summonerName);
+      console.log(data.data);
       this.summoner = data.data;
     } catch (err) {
       console.log(err);
     }
+  }
+
+  @action
+  async getSummonerLank(summonerId) {
+    try {
+      const data = await SummonerRepository.getSummonerLank(summonerId);
+
+      data.data.forEach((lank, index, array) => {
+        if (lank.queueType === "RANKED_TFT") {
+          array.splice(index, 1);
+        }
+      })
+
+      if (data.data.length === 0) {
+        data.data = [{ tier: "UNRANKED" },
+        { tier: "UNRANKED" }];
+      } else if (data.data.length === 1) {
+        data.data = [
+          ...data.data,
+          { tier: "UNRANKED" }];
+      }
+
+      this.summonerLank = data.data;
+      this.loading = true;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  @action
+  async getSummonerMatches(summonerAccountId) {
+    try {
+      const data = await SummonerRepository.getSummonerMatches(summonerAccountId)
+      let matches = data.data.matches.splice(0, 5);
+
+        let match = matches.map(async (match) => {
+            const data =  await SummonerRepository.getSummonerMatchesStat(match.gameId);
+            data.data = {
+              ...data.data,
+              gameDuration : this.getMiniute(data.data.gameDuration)
+            };
+
+            return({
+              ...match, ...data.data
+            });
+          });
+
+          Promise.all(match).then((completed) => {
+            this.matches = completed;
+          })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async getSummonerMatchesStat(gameId) {
+    try {
+      const data = await SummonerRepository.getSummonerMatchesStat(gameId);
+
+      return data.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getMiniute = (second) => {
+    return Math.floor(second / 60);
+  }
+
+  getWinRatio(value1, value2) {
+    return Math.floor(value1 / (value1 + value2) * 100);
   }
 }
 
